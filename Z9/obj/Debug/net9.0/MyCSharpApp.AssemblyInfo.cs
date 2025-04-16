00998fad9a7cadf63fb60874e4ce804ef6332a33
BidStory <bids.stories@gmail.com>
2025-04-16 02:11:16 +0200

    yyy
    4545

--- Z9/obj/Debug/net9.0/MyCSharpApp.AssemblyInfo.cs
+++ Z9/obj/Debug/net9.0/MyCSharpApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a638f8704bea119ef9c542fbc648494af7a2e8a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85bc449013db5937880406b993d04cf9aa14e874")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
