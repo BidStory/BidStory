0032fe0779c00c395688ae397408ca8058d95a83
BidStory <bids.stories@gmail.com>
2025-04-22 22:12:06 +0200

    25

--- programC2GetData/obj/Debug/net9.0/MyCSharpApp.AssemblyInfo.cs
+++ programC2GetData/obj/Debug/net9.0/MyCSharpApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+587c92db5ee5ec920a7e82a65bf78fefc88160b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e3ec29eb12aadf1264daafc97ab368e5af1419f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
