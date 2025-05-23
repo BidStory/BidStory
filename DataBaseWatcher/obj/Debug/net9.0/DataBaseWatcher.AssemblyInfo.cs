c7398d18b52f0cd4a33207e23a3cca8637cbf441
BidStory <bids.stories@gmail.com>
2025-05-23 21:49:05 +0300

    25

--- DataBaseWatcher/obj/Debug/net9.0/DataBaseWatcher.AssemblyInfo.cs
+++ DataBaseWatcher/obj/Debug/net9.0/DataBaseWatcher.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataBaseWatcher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45e9adcc688b676b62e78ab67bf3f8e318e85852")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5d8d7813a8b920de82d8c13c400d80b78b9d257")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataBaseWatcher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataBaseWatcher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
